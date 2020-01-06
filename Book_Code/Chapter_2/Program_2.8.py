ac3543719a83dfce9edc4b649336c3293c1df5b0
purvinis <g.purvinis@gmail.com>
2020-01-05 14:43:27 -1000

    Update Program_2.8.py
    Error line 5: used wrong variable

--- Book_Code/Chapter_2/Program_2.8.py
+++ Book_Code/Chapter_2/Program_2.8.py
@@ -2,5 +2,5 @@
 
 int_to_string = str(8)
 float_to_string = str(3.5)
-print(f"After Integer to String Casting the result is {float_to_string}")
+print(f"After Integer to String Casting the result is {int_to_string}")
 print(f"After Float to String Casting the result is {float_to_string}")
